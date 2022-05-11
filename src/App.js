bdbc6fde3c22fdce6bb9779d3b53b225e9654e6c
ui-cod-tv <uicodetv@googlemail.com>
2022-05-11 21:20:26 +0100

    first rev

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { blogList } from "./blog-list.js";
+import { motion, Variants } from "framer-motion";
 
-function App() {
+const imageAnimate={
+    offscreen:{x:-100, opacity:0},
+    onscreen:{
+    x:0,
+    opacity:1,
+    rotate:[0,10,0],
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1}
+  }
+
+}
+
+const textAnimate={
+    offscreen:{y:100, opacity:0},
+    onscreen:{y:0,
+    opacity:1,
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1}
+ }
+
+}
+
+function Card({ image, h2, p,id }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div className="card" id={id}
+        initial={"offscreen"}
+        whileInView={"onscreen"}
+        viewport={{once:false, amount:0.5}}
+        transition={{staggerChildren:0.5}}
+    >
+      <motion.div className="image-container"       
+        variants={imageAnimate}
+      >{image}</motion.div>
+      <motion.h2 
+        variants={textAnimate}
+      >{h2}</motion.h2>
+      <motion.p
+         variants={textAnimate}     
+      >{p}</motion.p>
+    </motion.div>
   );
 }
 
-export default App;
+export default function App() {
+  return blogList.map((item, index) => (
+      <div className="card-wrapper" key={index}>
+          <Card image={item.image} h2={item.h2} p={item.p}  />
+      </div>
+  ));
+}
+
---
+++ src/blog-list.js
@@ -0,0 +1,43 @@
+export const blogList = [
+    {
+      image: "🍅",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍊",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍋",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍐",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍏",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍅",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍆",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    },
+    {
+      image: "🍇",
+      h2: "Fruit and vegetables",
+      p: "They are reallly good for you...."
+    }
+  ];
+  
